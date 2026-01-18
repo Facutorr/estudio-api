1410cb9f2bfc51db64f6f9dd07ef3c2d421d3dd6
Fernando Torres <estudiofernandotorres@gmail.com>
2026-01-18 17:32:29 -0300

    Add SMTP timeouts

--- src/services/mailer.ts
+++ src/services/mailer.ts
@@ -23,7 +23,10 @@ function getTransport() {
     host,
     port,
     secure: port === 465,
-    auth: { user, pass }
+    auth: { user, pass },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000
   })
 
   return cachedTransport
